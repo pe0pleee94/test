289eb168134c76648fa8c7b24338e95c1829fe31
maulid.diansyah@gmail.com <maulid.diansyah@gmail.com>
2023-07-01 00:39:03 +0700

    resume - controller add

---
+++ src/main/java/id/co/maybank/digitallending/common/BusinessTypeConst.java
@@ -0,0 +1,8 @@
+package id.co.maybank.digitallending.common;
+
+public class BusinessTypeConst {
+	
+	public static final String INDIVIDUAL = "INDIVIDUAL";
+	public static final String CORPORATE = "CORPORATE";
+
+}
---
+++ src/main/java/id/co/maybank/digitallending/request/dto/ResumeResponse.java
@@ -0,0 +1,14 @@
+package id.co.maybank.digitallending.request.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ResumeResponse {
+	
+	@Getter @Setter
+	private String messageId;
+	
+	@Getter @Setter
+	private String messageEn;
+
+}
